ab51b5a38468803ae4034363bb140f51e8d34db0
Chinwendu joy Enyidiegwu <chinwe640@gmail.com>
2023-03-06 22:52:56 +0100

    clean up

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './login.css'
-import { BrowserRouter as Router, Link } from 'react-router-dom'
+import {  Link } from 'react-router-dom'
 export default function Login() {
   return (
 		<div className="login">
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './register.css'
-import { BrowserRouter as Router, Link } from 'react-router-dom'
+import {  Link } from 'react-router-dom'
 export default function Register() {
   return (
 		<div
